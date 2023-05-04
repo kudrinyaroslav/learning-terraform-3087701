66c053ad33cbc809dfc102bc4a6a2a95634c9d22
kudrinyaroslav <51086405+kudrinyaroslav@users.noreply.github.com>
2023-05-04 12:18:14 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "blog-sg" {
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
-  egress_cidr_blocks = [0.0.0.0/0"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
   
 data "aws_vpc" "default" {

efd2616bbf8d80546d80780e945bfbd0219304be
kudrinyaroslav <51086405+kudrinyaroslav@users.noreply.github.com>
2023-05-04 12:24:11 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,25 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-  vpc_security_group_ids = [module.blog-sg.security_group_id]
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
-module "blog-sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.2"
-  name = "blog_new"
-  vpc_id = data.aws_vpc.default.id
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
   
 data "aws_vpc" "default" {
     default = true

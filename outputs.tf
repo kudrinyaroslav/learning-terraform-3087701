de0e8a614d7dabc09e5f00b422901c4888482338
kudrinyaroslav <51086405+kudrinyaroslav@users.noreply.github.com>
2023-05-04 12:24:27 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,1 @@
-output "instance_ami" {
-  value = aws_instance.web.ami
-}
 
-output "instance_arn" {
-  value = aws_instance.web.arn
-}
-
-output "instance_dns" {
-  value = aws_instance.web.public_dns
-}
